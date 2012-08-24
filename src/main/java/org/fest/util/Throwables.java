841ad7153de858d32d42835cc30fe9f64ceb6189
Alex Ruiz <alruiz@google.com>
2012-08-23 23:20:17 -0700

    Minor cleanup.

--- src/main/java/org/fest/util/Throwables.java
+++ src/main/java/org/fest/util/Throwables.java
@@ -1,33 +1,32 @@
 /*
  * Created on Dec 13, 2008
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * Copyright @2008-2012 the original author or authors.
  */
 package org.fest.util;
 
 import static org.fest.util.Lists.newArrayList;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Utility methods related to <code>{@link Throwable}</code>s.
- * 
+ *
  * @author Alex Ruiz
  */
 public final class Throwables {
   /**
    * Appends the stack trace of the current thread to the one in the given <code>{@link Throwable}</code>.
-   * 
+   *
    * @param t the given {@code Throwable}.
    * @param methodToStartFrom the name of the method used as the starting point of the current thread's stack trace.
    */
@@ -51,13 +50,13 @@ public final class Throwables {
   }
 
   private static List<StackTraceElement> stackTraceInCurrentThread() {
-    return newArrayList(StackTraces.instance().stackTraceInCurrentThread());
+    return newArrayList(Thread.currentThread().getStackTrace());
   }
 
   /**
    * Removes the FEST-related elements from the <code>{@link Throwable}</code> stack trace that have little value for
    * end user. Therefore, instead of seeing this:
-   * 
+   *
    * <pre>
    * org.junit.ComparisonFailure: expected:<'[Ronaldo]'> but was:<'[Messi]'>
    *   at sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
@@ -73,9 +72,9 @@ public final class Throwables {
    *   at org.fest.assertions.api.AbstractAssert.isEqualTo(AbstractAssert.java:74)
    *   at examples.StackTraceFilterExample.main(StackTraceFilterExample.java:13)
    * </pre>
-   * 
+   *
    * We get this:
-   * 
+   *
    * <pre>
    * org.junit.ComparisonFailure: expected:<'[Ronaldo]'> but was:<'[Messi]'>
    *   at sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
