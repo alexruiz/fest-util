ca3a8f44f0c9fad4e32b98e0ba52c6deed886e2e
Alex Ruiz <alruiz@google.com>
2012-09-04 01:45:41 -0700

    CLOSED - task 103: Remove dependencies on AWT https://github.com/alexruiz/fest-assert-2.x/issues/issue/103
    Forgot to remove a reference to AWT Dimension.

--- src/main/java/org/fest/util/ToString.java
+++ src/main/java/org/fest/util/ToString.java
@@ -1,15 +1,15 @@
 /*
  * Created on Oct 7, 2009
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * Copyright @2009-2012 the original author or authors.
  */
 package org.fest.util;
@@ -19,15 +19,11 @@ import static org.fest.util.Strings.quote;
 
 import java.awt.Dimension;
 import java.io.File;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Obtains the {@code toString} representation of an object.
- * 
+ *
  * @author Alex Ruiz
  * @author Joel Costigliola
  * @author Yvonne Wang
@@ -36,7 +32,7 @@ public final class ToString {
   /**
    * Returns the {@code toString} representation of the given object. It may or not the object's own implementation of
    * {@code toString}.
-   * 
+   *
    * @param o the given object.
    * @return the {@code toString} representation of the given object.
    */
@@ -63,7 +59,7 @@ public final class ToString {
       return toStringOf((Long) o);
     }
     if (o instanceof Dimension) {
-      return toStringOf((Dimension) o);
+      return toStringOf(o);
     }
     if (o instanceof File) {
       return toStringOf((File) o);
@@ -107,10 +103,6 @@ public final class ToString {
 
   private static String toStringOf(Long l) {
     return String.format("%sL", l);
-  }
-
-  private static String toStringOf(Dimension d) {
-    return String.format("(w=%s, h=%s)", d.width, d.height);
   }
 
   private static String toStringOf(File f) {
